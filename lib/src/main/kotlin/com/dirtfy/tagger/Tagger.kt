78fd79073d1f3336452c1e462377cf5028c73863
Dirtfy <irsam78@gmail.com>
2025-01-26 01:09:33 +0900

    javadoc add

--- lib/src/main/kotlin/com/dirtfy/tagger/Tagger.kt
+++ lib/src/main/kotlin/com/dirtfy/tagger/Tagger.kt
@@ -1,7 +1,13 @@
 package com.dirtfy.tagger
 
+/**
+ * This is a auto tagger interface. It has TAG as property
+ */
 interface Tagger {
 
+    /**
+     * The function name in the tag might differ from what you expect because it is derived from the current thread's stack trace.
+     */
     val TAG: String
         get() {
             return this::class.simpleName + "." +
